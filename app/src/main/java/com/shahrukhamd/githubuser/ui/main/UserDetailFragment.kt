d62bbe94a01585a6611329a71369d3399907e2c1
Shahrukh Ahmed Siddiqui <shahrukh.amd@gmail.com>
2021-08-08 15:29:37 +0800

    feat: added navigation component

---
+++ app/src/main/java/com/shahrukhamd/githubuser/ui/main/UserDetailFragment.kt
@@ -0,0 +1,15 @@
+/*
+ * Copyright (c) 2021 Shahrukh Ahmed Siddiqui
+ *
+ * You may use, distribute and modify this code under the
+ * terms of the MIT license - https://opensource.org/licenses/MIT
+ */
+
+package com.shahrukhamd.githubuser.ui.main
+
+import androidx.fragment.app.Fragment
+import com.shahrukhamd.githubuser.R
+
+class UserDetailFragment: Fragment(R.layout.fragment_user_details) {
+
+}
